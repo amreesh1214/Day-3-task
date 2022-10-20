f87597c13ab3714883627f03ac21c02511c533a1
amreesh1214 <116099919+amreesh1214@users.noreply.github.com>
2022-10-20 23:25:07 +0530

    Add files via upload

---
+++ day 3 for.js
@@ -0,0 +1,21 @@
+let resume = 
+
+
+[
+    
+
+    {
+   "carrer objective" : "Highly motivated self starter with fully commited to providing myself in a new career as software engineer"
+},
+{
+   "Education" : "B.E Electronics and Instrumentation Engineering"
+},
+{
+   "Languages known" : "Tamil,English,Hindi"
+}
+
+
+]
+for(let i=0; i<resume.length; i++){
+    console.log(resume[i])
+}
---
+++ day 3 forin.js
@@ -0,0 +1,16 @@
+let resume = 
+[{
+   "carrer objective" : "Highly motivated self starter with fully commited to providing myself in a new career as software engineer"
+},
+{
+   "Education" : "B.E Electronics and Instrumentation Engineering"
+},
+{
+   "Languages known" : "Tamil,English,Hindi"
+}
+
+
+]
+for (let key in resume){
+console.log(resume)
+}
